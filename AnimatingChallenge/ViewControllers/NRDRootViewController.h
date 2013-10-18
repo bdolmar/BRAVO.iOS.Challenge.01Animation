48f35da78b6cded5ae5e3e6e61e63e06b165d304
Joshua Sullivan <jsulliva@nerdery.com>
2013-10-18 15:50:22 -0500

    Completed challenge.
    I went with a custom CALayer subclass that would report the progress of
    an animation on a custom property it implemented to its delegate. From
    there I simply used the progress to compute the number that should be
    displayed.

---
+++ AnimatingChallenge/ViewControllers/NRDRootViewController.h
@@ -0,0 +1,13 @@
+//
+//  NRDRootViewController.h
+//  AnimatingChallenge
+//
+//  Created by Joshua Sullivan on 10/16/13.
+//  Copyright (c) 2013 The Nerdery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NRDRootViewController : UIViewController
+
+@end
